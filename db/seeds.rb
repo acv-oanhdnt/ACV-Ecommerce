c74ef10289f8c825903344e60cc2429430449225
acv-oanhdnt <oanhdnt@atmarkcafe.org>
2018-08-23 15:23:56 +0700

    built layouts and implement products page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
 Category.delete_all
 Product.delete_all
 
-category = Category.create!(name: Faker::GameOfThrones.house)
-9.times do
-  category.products << Product.create(
-    name: Faker::Food.dish,
-    description: Faker::Food.description,
-    price: Faker::Number.decimal(3, 2),
-    img_url: Faker::Avatar.image
-  )
+
+5.times do
+  category = Category.create!(name: Faker::GameOfThrones.house)
+  12.times do
+    category.products << Product.create(
+      name: Faker::Food.dish,
+      description: Faker::Food.description,
+      price: Faker::Number.decimal(3, 2),
+      img_url: Faker::Avatar.image
+    )
+  end
 end
