6996203b0302bc8709bc93743bc27b4fe081812e
acv-oanhdnt <oanhdnt@atmarkcafe.org>
2018-08-23 15:57:28 +0700

    Add Kaminari to Paginate

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.page(params[:page]).per(12)
   end
   def show
     @product = Product.find{params[:id]}
