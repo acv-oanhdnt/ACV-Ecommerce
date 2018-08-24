0b89ac4f03972760fa027b7d325c5f18810ca052
Do Ngoc Thuy Oanh <dongocthuyoanh@Dos-MacBook-Air.local>
2018-08-24 10:18:32 +0700

    finish editing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all.page(params[:page]).per(12)
   end
+
   def show
     @product = Product.find{params[:id]}
   end
