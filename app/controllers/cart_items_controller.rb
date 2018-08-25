b6e399d47b7d9de35bd05b4de1a33da4ad6b81de
acv-oanhdnt <oanhdnt@atmarkcafe.org>
2018-08-25 18:15:05 +0700

    Finished add to cart function in show product page

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,22 @@
+class CartItemsController < ApplicationController
+    def create
+      cart = current_cart
+      product = Product.find(params[:product_id])
+
+      selected_item = cart.cart_items.where(product_id: product.id).first
+
+      if selected_item
+        selected_item.quantity += params[:quantity].to_i
+      else
+        selected_item = cart.cart_items.build(quantity: params[:quantity], price: product.price, product_id: product.id)
+      end
+
+      respond_to do |format|
+        if selected_item.save
+          format.html {redirect_to product_path(product.id)}
+        else
+          format.html {redirect_to product_path(product.id), notice: 'Error when adding item to cart.'}
+        end
+      end
+  end
+end
